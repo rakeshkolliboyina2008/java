7cd0748717265b2570c2487d93047c5925b4596c
rakeshkolliboyina2008 <rakeshkolliboyina2008@gmail.com>
2026-08-29 19:28:41 +0530

    Refactor Sample.java for clarity and structure

--- Assignment Programs/assignment-2/Types of Multipleinhertance/Sample.java
+++ Assignment Programs/assignment-2/Types of Multipleinhertance/Sample.java
@@ -1,17 +1,21 @@
 package interfaces;
+//first inheritance
 interface book{
 	 int x=10;
 	void name();
 }
+//second inheritance
 interface  card{
 	int z=15;
 	void num();
 }
+//parent class
 class ram{
 	void show() {
 		System.out.println("shoe the data");
 	}
 }
+//sub class
 class raj extends ram implements book,card{
 	@Override
 	public void name() {
