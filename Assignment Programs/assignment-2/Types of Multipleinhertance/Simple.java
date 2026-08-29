8e32cd265fdec071721ef05592f4956eec903fad
rakeshkolliboyina2008 <rakeshkolliboyina2008@gmail.com>
2026-08-29 19:30:10 +0530

    Enhance Simple.java with comments on inheritance
    Added comments to clarify inheritance structure in Simple.java.

--- Assignment Programs/assignment-2/Types of Multipleinhertance/Simple.java
+++ Assignment Programs/assignment-2/Types of Multipleinhertance/Simple.java
@@ -1,12 +1,15 @@
 package interfaces;
+//first inheritance
 interface riya{
 	 int x=10;
 	void siva();
 }
+//second inheritance
 interface raji extends riya {
 	int z=15;
 	void lucky();
 }
+//inheritance
 class Raghu implements raji{
 	@Override
 	 public void siva() {
